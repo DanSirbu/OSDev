e74e68b7f0a0a27cab7aad628f4ec5f5ea70a836
Dan Sirbu <dsirb066@gmail.com>
2018-10-21 08:24:37 -0400

    Refactored itoa

--- lib/string.c
+++ lib/string.c
@@ -19,25 +19,27 @@ void reverse(char *str) {
 		end--;
 	}
 }
-void itoa(u64 number, char *str, u32 base) {
+#define ASCII_NUMBER_CONST 0x30
+#define ASCII_LETTER_CONST 0x57
+void itoa(u32 number, char *str, u32 base) {
 	if(number == 0) {
 		str[0] = '0';
 		str[1] = '\0';
 		return;
 	}
 	u32 i = 0;
-	u32 lowBytes = number & 0xFFFFFFFF;
+	u32 lowBytes = number; //& 0xFFFFFFFF;
 	while(lowBytes != 0) {
-		u64 rem = lowBytes % base;
+		u32 rem = lowBytes % base;
 		if(rem < 10) {
-			str[i] = rem + 0x30;
+			str[i] = rem + ASCII_NUMBER_CONST;
 		} else {
-			str[i] = rem + 0x57;
+			str[i] = rem + ASCII_LETTER_CONST;
 		}
 		lowBytes /= base;
 		i++;
 	}
-	u32 highBytes = (number >> 31) >> 1 & 0xFFFFFFFF; //>> 32, but IA-32 processors mask shift to 5 bits so max is shift is 31
+	/*u32 highBytes = (number >> 31) >> 1 & 0xFFFFFFFF; //>> 32, but IA-32 processors mask shift to 5 bits so max is shift is 31
 	while(highBytes != 0) {
 		u64 rem = highBytes % base;
 		if(rem < 10) {
@@ -47,7 +49,7 @@ void itoa(u64 number, char *str, u32 base) {
 		}
 		highBytes /= base;
 		i++;
-	}
+	}*/
 	str[i] = '\0';
 
 	reverse(str);
