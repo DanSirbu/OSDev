daae94c613b3b66c4e68b1c637ac779f7fad7e9d
Dan Sirbu <dsirb066@gmail.com>
2018-11-02 20:07:25 -0400

    Added assert for easier debugging

--- include/serial.h
+++ include/serial.h
@@ -6,3 +6,18 @@
 void init_serial();
 void kpanic_fmt(char *message, ...);
 void kpanic(char *message);
+
+#define assert(statement)                                                      \
+	(statement) ? (void)0 : assert_failed(__FILE__, __LINE__, #statement)
+
+#define fail(why) assert_failed(__FILE__, __LINE__, why)
+
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+
+static inline void assert_failed(char *file, uint32_t line,
+				 const char *statement)
+{
+	kpanic_fmt("Error in %s:%d: " ANSI_COLOR_RED "%s" ANSI_COLOR_RESET "\n",
+		   file, line, statement);
+}
