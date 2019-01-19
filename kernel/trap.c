e93fc0d8afd951ebbeaad635c506f76f102d9cb4
Dan Sirbu <dsirb066@gmail.com>
2019-01-18 22:04:01 -0500

    Added sanity check when printing generic interrupt message

--- kernel/trap.c
+++ kernel/trap.c
@@ -64,12 +64,12 @@ void register_isr_handler(int interrupt_no, isr_handler_t handler)
 void interrupt_handler(int_regs_t regs)
 {
 	if (regs.interrupt_no < 32) {
-		debug_print("Exception %d (%s) at 0x%x, error %d\n",
+		debug_print("Exception %d (%s) at 0x%x, error 0x%x\n",
 			    regs.interrupt_no,
 			    exceptions_string[regs.interrupt_no], regs.eip,
 			    regs.error_code);
-	} else if (regs.interrupt_no != 32) {
-		debug_print("Interrupt %d (%s) at 0x%x, error %d\n",
+	} else if (regs.interrupt_no != 32 && (regs.interrupt_no - 32) < sizeof(interrupts_string) / sizeof(interrupts_string[0])) {
+		debug_print("Interrupt %d (%s) at 0x%x, error 0x%x\n",
 			    regs.interrupt_no - 32,
 			    interrupts_string[regs.interrupt_no - 32], regs.eip,
 			    regs.error_code);
