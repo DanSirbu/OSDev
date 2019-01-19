c1fd6756a5cadab462ef86bcc772d82aa2b28fbe
Dan Sirbu <dsirb066@gmail.com>
2019-01-19 17:33:32 -0500

    Updated apps

--- apps/init.c
+++ apps/init.c
@@ -1,3 +1,13 @@
+static inline int fork()
+{
+	int ret;
+	asm("movl $1, %%eax; int $0x80; mov %%eax, %[retval]"
+	    : [retval] "=r"(ret)
+	    :
+	    : "eax");
+
+	return ret;
+}
 int test(int y)
 {
 	return y + 10;
@@ -6,4 +16,11 @@ void main()
 {
 	int x = 1;
 	x = test(x) * 0x222;
+
+	int y = fork();
+	if (y == 0) {
+		while (1) {
+			int z = 0x999;
+		}
+	}
 }
