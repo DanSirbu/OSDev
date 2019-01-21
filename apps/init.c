7771494cfe5d260ed6d23f7be7879a90c45fa036
Dan Sirbu <dsirb066@gmail.com>
2019-01-21 18:36:42 -0500

    Removed "syscall_" prefix

--- apps/init.c
+++ apps/init.c
@@ -9,7 +9,7 @@ void main()
 	int x = 1;
 	x = test(x) * 0x222;
 
-	int y = syscall_fork();
+	int y = fork();
 	if (y == 1) {
 		while (1) {
 			int z = 0x999;
--- libc/sys.h
+++ libc/sys.h
@@ -4,22 +4,26 @@
 //ebp is saved by the function prologue
 //%eax for syscall_number. parameters: %ebx, %ecx, %edx, %esi, %edi, %ebp
 //so we have to save ebx, esi, edi
-#define DEF_SYSCALL0(fn, name) \
-    int syscall_##name() { \
-        return syscall(fn, 0, 0, 0, 0, 0); \
-    }
-#define DEF_SYSCALL1(fn, name, P1, p1) \
-    int syscall_##name(P1 p1) { \
-        return syscall(fn, (int) p1, 0, 0, 0, 0); \
-    }
-#define DEF_SYSCALL2(fn, name, P1, p1, P2, p2) \
-    int syscall_##name(P1 p1, P2 p2) { \
-        return syscall(fn, (int) p1, (int) p2, 0, 0, 0); \
-    }
-#define DEF_SYSCALL3(fn, name, P1, p1, P2, p2, P3, p3) \
-    int syscall_##name(P1 p1, P2 p2, P3 p3) { \
-        return syscall(fn, (int) p1, (int) p2, (int) p3, 0, 0); \
-    }
+#define DEF_SYSCALL0(fn, name)                                                 \
+	int name()                                                             \
+	{                                                                      \
+		return syscall(fn, 0, 0, 0, 0, 0);                             \
+	}
+#define DEF_SYSCALL1(fn, name, P1, p1)                                         \
+	int name(P1 p1)                                                        \
+	{                                                                      \
+		return syscall(fn, (int)p1, 0, 0, 0, 0);                       \
+	}
+#define DEF_SYSCALL2(fn, name, P1, p1, P2, p2)                                 \
+	int name(P1 p1, P2 p2)                                                 \
+	{                                                                      \
+		return syscall(fn, (int)p1, (int)p2, 0, 0, 0);                 \
+	}
+#define DEF_SYSCALL3(fn, name, P1, p1, P2, p2, P3, p3)                         \
+	int name(P1 p1, P2 p2, P3 p3)                                          \
+	{                                                                      \
+		return syscall(fn, (int)p1, (int)p2, (int)p3, 0, 0);           \
+	}
 /*#define DEF_SYSCALL4(fn, name, p1, p2, p3, p4) \
     inline int syscall_##name() { \
         return syscall(fn, (int) p1, (int) p2, (int) p3, (int) p4, 0); \
@@ -29,9 +33,10 @@
         return syscall(fn, (int) p1, (int) p2, (int) p3, (int) p4, (int) p5); \
     }
 */
-static inline int syscall(int num, int p1, int p2, int p3, int p4, int p5) {
-    int ret;
-    asm ("push %%ebx; push %%esi; push %%edi; \
+static inline int syscall(int num, int p1, int p2, int p3, int p4, int p5)
+{
+	int ret;
+	asm("push %%ebx; push %%esi; push %%edi; \
     movl %1, %%eax; \
     movl %2, %%ebx; \
     movl %3, %%ecx; \
@@ -41,9 +46,9 @@ static inline int syscall(int num, int p1, int p2, int p3, int p4, int p5) {
     int $0x80; \
     movl %[retval], %%eax; \
     pop %%edi; pop %%esi; pop %%ebx"
-    : [retval] "=r" (ret)
-    : "0" (num), "g" (p1), "g"(p2), "g"(p3), "g"(p4), "g"(p5)
-    : "ebx", "esi", "edi", "eax");
+	    : [retval] "=r"(ret)
+	    : "0"(num), "g"(p1), "g"(p2), "g"(p3), "g"(p4), "g"(p5)
+	    : "ebx", "esi", "edi", "eax");
 
-    return ret;
+	return ret;
 }
