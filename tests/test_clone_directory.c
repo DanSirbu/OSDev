7a1469dc4348bad8dab30a5580948fedd43b612b
Dan Sirbu <dsirb066@gmail.com>
2019-01-22 09:51:48 -0500

    Added more tests for clone_directory

--- tests/test_clone_directory.c
+++ tests/test_clone_directory.c
@@ -7,16 +7,22 @@ void test_clone_directory()
 	page_directory_t *newdir = clone_directory(current_directory);
 	assert(newdir != current_directory);
 
-	for (int x = 0; x < 1024; x++) {
+	for (uint32_t x = 0; x < 1024; x++) {
 		assert(newdir->actual_tables[x].present ==
 		       current_directory->actual_tables[x].present);
+		
+		if(x >= KERN_BASE_PAGE_NO) {
+			continue;
+		}
 
 		if (newdir->actual_tables[x].present) {
+			assert(newdir->actual_tables[x].frame != current_directory->actual_tables[x].frame);
 			for (int i = 0; i < 1024; i++) {
 				assert(newdir->tables[x]->pages[i].present ==
 				       current_directory->tables[x]
 					       ->pages[i]
 					       .present);
+				assert(newdir->tables[x]->pages[i].frame != current_directory->tables[x]->pages[i].frame);
 			}
 		}
 	}
