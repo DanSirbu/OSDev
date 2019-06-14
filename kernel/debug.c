8f1a0165f3aeef649779463f3e3523852237a9fb
Dan Sirbu <dsirb066@gmail.com>
2019-06-14 15:38:16 -0400

    Added more syscall numbers

--- kernel/debug.c
+++ kernel/debug.c
@@ -2,32 +2,12 @@
 #include "serial.h"
 #include "coraxstd.h"
 
-#define NUM_SYSCALLS 71
-//TODO find a better way to do this
-char **syscall_names[NUM_SYSCALLS] = {
-	"__NR_setup",   "__NR_exit",    "__NR_fork",	 "__NR_read",
-	"__NR_write",   "__NR_open",    "__NR_close",	"__NR_waitpid",
-	"__NR_creat",   "__NR_link",    "__NR_unlink",       "__NR_execve",
-	"__NR_chdir",   "__NR_time",    "__NR_mknod",	"__NR_chmod",
-	"__NR_chown",   "__NR_break",   "__NR_stat",	 "__NR_lseek",
-	"__NR_getpid",  "__NR_mount",   "__NR_umount",       "__NR_setuid",
-	"__NR_getuid",  "__NR_stime",   "__NR_ptrace",       "__NR_alarm",
-	"__NR_fstat",   "__NR_pause",   "__NR_utime",	"__NR_stty",
-	"__NR_gtty",    "__NR_access",  "__NR_nice",	 "__NR_ftime",
-	"__NR_sync",    "__NR_kill",    "__NR_rename",       "__NR_mkdir",
-	"__NR_rmdir",   "__NR_dup",     "__NR_pipe",	 "__NR_times",
-	"__NR_prof",    "__NR_brk",     "__NR_setgid",       "__NR_getgid",
-	"__NR_signal",  "__NR_geteuid", "__NR_getegid",      "__NR_acct",
-	"__NR_phys",    "__NR_lock",    "__NR_ioctl",	"__NR_fcntl",
-	"__NR_mpx",     "__NR_setpgid", "__NR_ulimit",       "__NR_uname",
-	"__NR_umask",   "__NR_chroot",  "__NR_ustat",	"__NR_dup2",
-	"__NR_getppid", "__NR_getpgrp", "__NR_setsid",       "__NR_sbrk",
-	"__NR_clone",   "__NR_seek",    "__NR_update_screen"
-};
+extern size_t SYSCALL_NAMES_SIZE;
+extern char *syscall_names[];
 
 char *getSyscallName(size_t number)
 {
-	if (number < NUM_SYSCALLS) {
+	if (number < SYSCALL_NAMES_SIZE) {
 		return syscall_names[number];
 	}
 	return NULL;
